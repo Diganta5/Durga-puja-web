101e623a314b532c9ce00dea8ba95e3e9721bf26
Diganta5 <digantasamanta2@gmail.com>
2024-09-30 23:38:53 +0530

    first commit

--- countdown.js
+++ countdown.js
@@ -37,9 +37,8 @@ const timerInterval = setInterval(countdown, 1000);
 const mobileMenu = document.getElementById('mobile-menu');
 const navLinks = document.getElementById('nav-links');
 
-// Add event listener to toggle the mobile menu
 mobileMenu.addEventListener('click', () => {
     navLinks.classList.toggle('active');
-    const isExpanded = navLinks.classList.contains('active');
-    mobileMenu.setAttribute('aria-expanded', isExpanded);  // Accessible toggle
+    mobileMenu.classList.toggle('active');
 });
+
